f6667730e18c6abad0fa2b1dcd0b2feccb1224fb
Tegar <tegaradityansyah07@gmail.com>
2024-07-05 09:24:58 +0700

    all comiit

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 //LOGIN
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('postLogin');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 //
 Route::get('/admin', [AdminController::class, 'index']);
 
