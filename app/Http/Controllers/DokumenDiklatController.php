0436b3411c14948454504636704f49a099e11fca
malikah <siswiian@gmail.com>
2024-07-07 14:48:52 -0700

    malikah

--- app/Http/Controllers/DokumenDiklatController.php
+++ app/Http/Controllers/DokumenDiklatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\dokumen_diklat;
+use App\Models\jenis_dokumen;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -12,43 +13,68 @@ class DokumenDiklatController extends Controller
     {
         $menu='data';
         $submenu='dokumen_diklat';
-        $datas = dokumen_diklat::latest()->paginate(5);
+        $datas = dokumen_diklat::join('jenis_dokumens', 'jenis_dokumen_id', '=', 'jenis_dokumens.id')->select("file", "nama_dokumen", "keterangan", "jenis_dokumen_id", "link", "dokumen_diklats.id AS id")->paginate(10);
         return view('pages.admin.dokumen_diklat.index', compact('datas', 'menu', 'submenu'));
     }
+
+    public function create()
+    {
+        $menu = 'data';
+        $submenu = 'dokuemn_diklat';
+        $jenis_dokumen = jenis_dokumen::all();
+        return view('pages.admin.dokumen_diklat.form', compact('menu', 'submenu', 'jenis_dokumen'));
+    }
+
     public function store(Request $request)
     {
         $file = $request->file('file');
-        $file->storeAs('public/pages.dokumen_diklat', $file->hashName());
+        $file->storeAs('admin/dokumen_diklat', $file->hashName());
+
+
+
         dokumen_diklat::create([
             'file'=>$file->hashName(),
             'nama_dokumen'=>$request->nama_dokumen,
             'keterangan'=>$request->keterangan,
+            'jenis_dokumen_id'=>$request->jenis_dokumen_id,
+            'link'=>$request->link,
         ]);
-        return redirect()->route('pages.dokumen_diklat.index')->with('success', 'Data Posts berhasil di simpan.');
+        return redirect()->route('pages.dokumen_diklat.index')->with('success', 'Dokumen Diklat berhasil di simpan.');
     }
-    public function show($id)
-    {
-        $menu = 'data';
-        $submenu = 'dokumen_diklat';
-        $dokumen_diklat = dokumen_diklat::find($id);
-        return view('pages.admin.dokumen_diklat.show', compact('dokumen_diklat', 'menu', 'submenu'));
-    }
+
+    // public function show($id)
+    // {
+    //     $menu = 'data';
+    //     $submenu = 'dokumen_diklat';
+    //     $dokumen_diklat = dokumen_diklat::find($id);
+    //     return view('pages.admin.dokumen_diklat.show', compact('dokumen_diklat', 'menu', 'submenu'));
+    // }
+
     function edit($id){
         $menu = 'data';
         $submenu = 'dokumen_diklat';
         $dokumen_diklat = dokumen_diklat::find($id);
-        return view('pages.admin.dokumen_diklat.form_edit', compact('dokumen_diklat', 'menu', 'submenu'));
+        $jenis_dokumen = jenis_dokumen::all();
+        return view('pages.admin.dokumen_diklat.form_edit', compact('menu', 'submenu', 'dokumen_diklat', 'jenis_dokumen'));
     }
-    function update(Request $request, $id)
+
+    function update(Request $request, string $id)
     {
         $this->validate($request,[
-            'name'=>'required|min:5'
+            'jenis_dokumen_id'=>'required'
         ]);
         $dokumen_diklat = dokumen_diklat::findOrFail($id);
-        $dokumen_diklat-> update(['name'=>$request->name]);
+        $dokumen_diklat-> update([
+            'file'=>$request->file,
+            'nama_dokumen'=>$request->nama_dokumen,
+            'keterangan'=>$request->keterangan,
+            'jenis_dokumen_id'=>$request->jenis_dokumen_id,
+            'link'=>$request->link,
+        ]);
+        
         return redirect()->route('dokumen_diklat.index')->with(['success'=>'Data berhasil diubah!']);
     }
-    function destroy( $id)
+    function destroy(string $id)
     {
         $dokumen_diklat= dokumen_diklat::findOrFail($id);
         $dokumen_diklat->delete();
