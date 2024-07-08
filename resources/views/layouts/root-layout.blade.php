ea0b2b990eb43f4e8a37de16080351fdf98a01a8
maulana adam <wandgenius6@gmail.com>
2024-07-08 14:20:30 +0700

    fixing datadisplayer in navbar

--- resources/views/layouts/root-layout.blade.php
+++ resources/views/layouts/root-layout.blade.php
@@ -60,13 +60,13 @@
                         data-bs-toggle="dropdown">
                         <img src="/assets/themes/nice/assets/img/profile-img.jpg" alt="Profile"
                             class="rounded-circle">
-                        <span class="d-none d-md-block dropdown-toggle ps-2">@yield('username')</span>
+                        <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
                     </a><!-- End Profile Iamge Icon -->
 
                     <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                         <li class="dropdown-header">
-                            <h6>@yield('username')</h6>
-                            <span>@yield('user-role')</span>
+                            <h6>{{ Auth::user()->name }}</h6>
+                            <span>{{ Auth::user()->role_id == 1 ? 'Admin' : 'Guru' }}</span>
                         </li>
                         <li>
                             <hr class="dropdown-divider">
