219436a0d4bae6b8faf7290267596c2b4bfcea0d
malikah <siswiian@gmail.com>
2024-07-15 19:00:02 -0700

    malikah

--- resources/views/pages/admin/dokumen_diklat/index.blade.php
+++ resources/views/pages/admin/dokumen_diklat/index.blade.php
@@ -5,7 +5,7 @@
     <h1>Dokumen Diklat</h1>
     <nav>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
             <li class="breadcrumb-item active">Dokumen Diklat</li>
         </ol>
     </nav>
